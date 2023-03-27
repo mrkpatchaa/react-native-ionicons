8f1bc459a475585f8db70dd41561f8343c4af7b3
Médédé Raymond KPATCHAA <kraymond70@gmail.com>
2023-03-27 01:25:56 +0000

    Upgrade to ionicons 7.1.0

---
+++ src/Icons/chevron-collapse-sharp.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+const SvgChevronCollapseSharp = props => <Svg xmlns="http://www.w3.org/2000/svg" className="chevron-collapse-sharp_svg__ionicon" viewBox="0 0 512 512" width={24} height={24} {...props}><Path d="M102.145 108.514 256 241.855l153.855-133.341-31.437-36.273L256 178.337 133.582 72.241l-31.437 36.273zm0 294.972L256 270.145l153.855 133.341-31.437 36.273L256 333.663 133.582 439.759l-31.437-36.273z" /></Svg>;
+export default SvgChevronCollapseSharp;
